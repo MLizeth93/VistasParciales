52f09b6d24563150800a99cc5de476321fbffe77
CDS012 <HP@CDS-AH-2011-012.sv.cds>
2018-09-26 09:48:51 -0600

    first commit

---
+++ Actividad3Mirna_G/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Actividad3Mirna_G
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Actividad3Mirna_G/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Actividad3Mirna_G.MvcApplication" Language="C#" %>
